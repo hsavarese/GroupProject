ed7156ff805d614fc5717be3dadc437894a2ccd1
Lekiko02 <156242189+Lekiko02@users.noreply.github.com>
2024-11-13 19:00:43 -0800

    Update DataManager.java

--- DocumentSimilaritySearch/src/main/java/backend/DataManager.java
+++ DocumentSimilaritySearch/src/main/java/backend/DataManager.java
@@ -1,12 +1,18 @@
 package backend;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-// import nlp.ExtractDocument;
+import nlp.ExtractDocument; 
 // import nlp.SimilarityCalculator;
 
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.List;
 
 public class DataManager {
@@ -15,17 +21,27 @@ public class DataManager {
 
         public DataManager() {
             try (MongoClient mongoClient = new MongoClient("mongodb+srv://denis:COS225@documentbase.6wttq.mongodb.net/?retryWrites=true&w=majority&appName=DocumentBase")) {
-                this.database = mongoClient.getDatabase("");
-                this.collection = database.getCollection("");
+                this.database = mongoClient.getDatabase("DocumentDB");
+                this.collection = database.getCollection("documents");
             }
         }
 
-    // Create operation: Upload a document and store its path and features
-    public void createDocument(String path) {
-        List<String> document = ExtractDocument.extractDocument(path);
-        Document doc = new Document("path", path).append("document", document);
+   // Upload a document's metadata to MongoDB
+    public void uploadWordFrequencies(Map<String, Integer> frequencyMap, String path) {
+        Document doc = new Document("path", path)
+                .append("wordFrequencies", frequencyMap);
+    
         collection.insertOne(doc);
-        System.out.println("Document uploaded to MongoDB successfully!");
+        System.out.println("Word frequencies uploaded to MongoDB successfully!");
+    }
+
+
+    public static void main(String[] args) {
+        // Define the path to the document file
+        String inputFile = "C:\\Users\\Sima\\OneDrive\\Documents\\DocumentSimilaritySearch\\src\\main\\java\\backend\\doc.txt";
+        
+        // Call the method to process the document and upload word frequencies to MongoDB
+        ExtractDocument.writeWordFrequency(inputFile);
     }
 }
 
