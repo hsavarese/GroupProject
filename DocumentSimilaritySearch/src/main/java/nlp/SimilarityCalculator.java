4077ac5d1ef29c3b12a69a58bfbbcefe6f8de85b
Hunter Savarese <157766362+hsavarese@users.noreply.github.com>
2024-12-13 19:58:06 -0500

    Update SimilarityCalculator.java

--- DocumentSimilaritySearch/src/main/java/nlp/SimilarityCalculator.java
+++ DocumentSimilaritySearch/src/main/java/nlp/SimilarityCalculator.java
@@ -7,6 +7,8 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.*;
 import java.util.stream.Collectors;
 
